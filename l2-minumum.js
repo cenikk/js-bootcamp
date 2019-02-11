de8f5dadc01f5f0a60be0be6612735b1101a5ba5
cenikk <k.cenik@live.nl>
2019-02-12 00:11:40 +0100

    Updated minimum.js and added flattening.js

--- l2-minumum.js
+++ l2-minumum.js
@@ -1,13 +1,13 @@
 // Write a function min that takes two arguments and returns their minimum.
 
-function returnMin(num1, num2) {
+function min(num1, num2) {
     var calcMin = (num1 > num2) ? num2 : num1; // Als num1 groter is dan num2 > return num2, anders return num1
     return calcMin;
 }
 
-console.log(returnMin(0, 10));
+console.log(min(0, 10));
 // → 0
-console.log(returnMin(0, -10));
+console.log(min(0, -10));
 // → -10
-console.log(returnMin(-99, -100));
+console.log(min(-99, -100));
 // → -100
